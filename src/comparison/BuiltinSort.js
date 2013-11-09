a407cc64e4f2a312c9f08e0d34be00e8d52739cc
Nikos M <Nikos@Nikos-PC.(none)>
2013-11-09 02:43:22 +0200

    updates

--- src/comparison/BuiltinSort.js
+++ src/comparison/BuiltinSort.js
@@ -5,10 +5,12 @@
     //
     
     // default built-in sort algorithm
+    // https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Array/sort
     Sort.BuiltinSort = function(a) {
         a.sort();
         // in-place
         return a;
     };
+    Sort.BuiltinSort.reference = "https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Array/sort";
     
 })(Sort);
